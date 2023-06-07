d7a2ee5a67bbba1e8d8db15b2f5e9a32935cb309
e-lab innovations <mohammedashad23@gmail.com>
2023-06-07 10:04:46 +0530

    function more readable

--- Pulse Shaping and Matched Filtering/srrc.m
+++ Pulse Shaping and Matched Filtering/srrc.m
@@ -6,11 +6,12 @@ function [response]=srrc(os_factor,roll_off)
     for i=1:1:length(t)
         if t(i)==0 
             p(i)= (1-a)+4*a/pi;
-        else if t(i)==1/(4*a) || t(i)==-1/(4*a) 
-            p(i)=a/sqrt(2)*((1+2/pi)*sin(pi/(4*a)+(1- 2/pi)*cos(pi/(4*a)))); 
         else
-            p(i) = (sin(pi*t(i)*(1-a))+4*a*t(i).*cos(pi*t(i)*(1+a)))./(pi*t(i).*(1-(4*a*t(i)).^2)); 
-        end
+            if t(i)==1/(4*a) || t(i)==-1/(4*a) 
+                p(i)=a/sqrt(2)*((1+2/pi)*sin(pi/(4*a)+(1- 2/pi)*cos(pi/(4*a)))); 
+            else
+                p(i) = (sin(pi*t(i)*(1-a))+4*a*t(i).*cos(pi*t(i)*(1+a)))./(pi*t(i).*(1-(4*a*t(i)).^2)); 
+            end
         end
     end
     response=p./sqrt(sum(p.^2)); %Normalization to unit energy
